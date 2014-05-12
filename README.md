71fbfd1fb0981bd14a8ec10cffed95c76ea737c9
bibig <liushuxun@gmail.com>
2014-05-12 20:14:43 +0800

    update doc

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # xun
 xun provides basic query functions for records-like array, like underscore.collection
+btw, xun is a chinese word '寻' means query.
 
 ### install
 npm install xun
--- test/basic.js
+++ test/basic.js
@@ -1,9 +1,9 @@
 var should = require('should');
 var assert = require('assert');
-var Query = require('../index');
+var Xun = require('../index');
 
 
-describe('test Query object', function () {
+describe('test Xun object', function () {
   var records = [
     {id: 1, name: 'Tom', age: 12}, 
     {id: 2, name: 'David', age: 9}, 
@@ -12,14 +12,14 @@ describe('test Query object', function () {
 
   
   it('create object', function () {
-    var query = Query.create(records);
-    assert.ok(typeof query == 'object');
+    var xun = Xun.create(records);
+    assert.ok(typeof xun == 'object');
   });
   
 
   it('test [=] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('id', 2).select();
+    var xun = Xun.create(records);
+    var results = xun.where('id', 2).select();
     // console.log(records);
     assert.ok(results.length == 1);
     results[0].should.have.property('id', 2);
@@ -28,124 +28,124 @@ describe('test Query object', function () {
   
 
   it('test [<] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('age', '<', 10).select();
+    var xun = Xun.create(records);
+    var results = xun.where('age', '<', 10).select();
     assert.ok(results.length == 1);
   });
   
   it('test [<=] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('age', '<=', 12).select();
+    var xun = Xun.create(records);
+    var results = xun.where('age', '<=', 12).select();
     // console.log(records);
     assert.ok(results.length == 2);
   });
   
   
   it('test [>=] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('age', '>=', 12).select();
+    var xun = Xun.create(records);
+    var results = xun.where('age', '>=', 12).select();
     // console.log(records);
     assert.ok(results.length == 2);
   });
 
   
   it('test [>] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('age', '>', 12).select();
+    var xun = Xun.create(records);
+    var results = xun.where('age', '>', 12).select();
     assert.ok(results.length == 1);
   }); 
   
   
   it('test [not in] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('name', 'not in', ['Tom', 'David']).order('age', 1).select('id', 'name', 'age');
+    var xun = Xun.create(records);
+    var results = xun.where('name', 'not in', ['Tom', 'David']).order('age', 1).select('id', 'name', 'age');
     assert.ok(results[0].name == 'Cici');
   });
   
   it('test [in] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('name', 'in', ['Tom']).order('age', 1).select('id', 'name', 'age');
+    var xun = Xun.create(records);
+    var results = xun.where('name', 'in', ['Tom']).order('age', 1).select('id', 'name', 'age');
     assert.ok(results[0].name == 'Tom');
   });
   
   it('test [between] operator', function () {
-    var query = Query.create(records);
-    var results = query.where('age', 'between', [8, 10]).select('id', 'name', 'age');
+    var xun = Xun.create(records);
+    var results = xun.where('age', 'between', [8, 10]).select('id', 'name', 'age');
     // console.log(results);
     assert.ok(results[0].name == 'David');
   });
   
   it('test order descend, select chain', function () {
-    var query = Query.create(records);
-    var results = query.order('age', true).select();
+    var xun = Xun.create(records);
+    var results = xun.order('age', true).select();
     // console.log(results);
     assert.ok(results[0].age == 14);
   });
   
   it('test order ascend', function () {
-    var query = Query.create(records);
-    var results = query.order('age').select();
+    var xun = Xun.create(records);
+    var results = xun.order('age').select();
     assert.ok(results[0].age == 9);
   });
   
   it('test select fields filter', function () {
-    var query = Query.create(records);
-    var results = query.select('name', 'age');
+    var xun = Xun.create(records);
+    var results = xun.select('name', 'age');
     results[0].should.have.property('name');
     results[0].should.have.property('age');
     results[0].should.not.have.property('id');
   });
   
   it('test limit', function () {
-    var query = Query.create(records);
-    var results = query.limit(2).select();
+    var xun = Xun.create(records);
+    var results = xun.limit(2).select();
     assert.ok(results.length == 2);
   });
   
   it('test sum', function () {
-    var query = Query.create(records);
-    var sum = query.where('age', '>', 9).sum('age');
+    var xun = Xun.create(records);
+    var sum = xun.where('age', '>', 9).sum('age');
     assert.ok(sum == 26);
   });
   
   it('test average', function () {
-    var query = Query.create(records);
-    var avg = query.where('age', '>', 9).average('age');
+    var xun = Xun.create(records);
+    var avg = xun.where('age', '>', 9).average('age');
     assert.ok(avg == 13);
   });
   
   it('test like ~%', function () {
-    var query = Query.create(records);
-    var results = query.where('name', 'like', 'Dav%').select();
+    var xun = Xun.create(records);
+    var results = xun.where('name', 'like', 'Dav%').select();
     assert.ok(results.length == 1);
     results[0].should.have.property('name', 'David');
   });
   
   it('test like %~', function () {
-    var query = Query.create(records);
-    var results = query.where('name', 'like', '%vid').select();
+    var xun = Xun.create(records);
+    var results = xun.where('name', 'like', '%vid').select();
     assert.ok(results.length == 1);
     results[0].should.have.property('name', 'David');
   });
   
   it('test like %~%', function () {
-    var query = Query.create(records);
-    var results = query.where('name', 'like', '%vi%').select();
+    var xun = Xun.create(records);
+    var results = xun.where('name', 'like', '%vi%').select();
     assert.ok(results.length == 1);
     results[0].should.have.property('name', 'David');
   });
   
   it('test regex', function () {
-    var query = Query.create(records);
-    var results = query.where('name', 're', /^\w{3}$/i).select();
+    var xun = Xun.create(records);
+    var results = xun.where('name', 're', /^\w{3}$/i).select();
     assert.ok(results.length == 1);
     results[0].should.have.property('name', 'Tom');
   });
   
   it('test filter', function () {
-    var query = Query.create(records);
-    // console.log(query.select());
-    var results = query.filter({
+    var xun = Xun.create(records);
+    // console.log(xun.select());
+    var results = xun.filter({
       age: ['>=', 9],
       name: ['like', 'To%']
     }).select();
